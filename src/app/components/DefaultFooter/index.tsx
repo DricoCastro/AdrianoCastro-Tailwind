392b04a8b0d7120fe7004388fdd0afe5556fd9b3
Adriano Castro <78217027+DricoCastro@users.noreply.github.com>
2023-06-21 07:20:02 -0300

    Feature Default Footer

---
+++ src/app/components/DefaultFooter/index.tsx
@@ -0,0 +1,15 @@
+export const DefaultFooter = () => {
+  return (
+    <div
+      className={`
+  w-full
+  bg-gray
+  flex
+  items-center
+  justify-center
+  `}
+    >
+      Footer
+    </div>
+  );
+};
