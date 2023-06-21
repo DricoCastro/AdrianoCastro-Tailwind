e15a795384d35e1cf83ea2f07dbe10d6c743f1db
Adriano Castro <78217027+DricoCastro@users.noreply.github.com>
2023-06-21 07:21:33 -0300

    Feature Me Section

---
+++ src/home/sections/me/me.tsx
@@ -0,0 +1,15 @@
+export const MeSection = () => {
+  return (
+    <div
+      className={`
+      flex
+      flex-row
+      items-center
+      justify-center
+      w-full
+  `}
+    >
+      ME
+    </div>
+  );
+};
